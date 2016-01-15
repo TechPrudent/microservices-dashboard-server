09b05c1b050507fef0436cef337b62a235abe01f
Sergei Storojenko <serguei.storojenko.ext@proximus.com>
2016-01-15 13:45:22 +0100

    deleted angular code

--- dependencies/src/main/angular-ui/e2e/bottombar.po.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var BottomBar = function() {
-  this.bottomBar = element(by.css('#bottombar'));
-  this.buttons = element.all(by.css('.button-div'));
-};
-
-module.exports = new BottomBar();
