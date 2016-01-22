39d9aabd7f89aca72d00ae064d6a4c8d804a683d
Sergei Storojenko <serguei.storojenko.ext@proximus.com>
2016-01-22 10:33:24 +0100

    removed configServer and discovery from the list of servers

--- dependencies/src/main/java/com/pxs/dependencies/aggregator/DependenciesListFilterPredicate.java
+++ dependencies/src/main/java/com/pxs/dependencies/aggregator/DependenciesListFilterPredicate.java
@@ -1,18 +1,19 @@
 package com.pxs.dependencies.aggregator;
 
-import java.util.List;
+import static com.pxs.dependencies.constants.Constants.CONFIGSERVER;
+import static com.pxs.dependencies.constants.Constants.DISCOVERY;
+import static com.pxs.dependencies.constants.Constants.DISK_SPACE;
+import static com.pxs.dependencies.constants.Constants.HYSTRIX;
 
 import com.google.common.base.Predicate;
 import com.pxs.dependencies.model.Node;
 
 public class DependenciesListFilterPredicate implements Predicate<Node> {
 
-	private static final String DISK_SPACE = "diskSpace";
-	private static final String HYSTRIX = "hystrix";
-
 	@Override
 	public boolean apply(Node input) {
-		return !HYSTRIX.equals(input.getId()) && !DISK_SPACE.equals(input.getId());
+		return !HYSTRIX.equals(input.getId()) && !DISK_SPACE.equals(input.getId())
+				&& !CONFIGSERVER.equals(input.getId()) && !DISCOVERY.equals(input.getId());
 	}
 }
 
