e08765a4ce3422a9f6b5d1a0258eb3a435889fa4
Sergei Storojenko <serguei.storojenko.ext@proximus.com>
2016-01-22 11:33:04 +0100

    removed configServer and discovery from the list of servers

--- dependencies/src/main/java/com/pxs/dependencies/constants/Constants.java
+++ dependencies/src/main/java/com/pxs/dependencies/constants/Constants.java
@@ -1,20 +1,17 @@
 package com.pxs.dependencies.constants;
 
-
 public class Constants {
 	public static final String MICROSERVICE = "MICROSERVICE";
 	public static final String TOOLBOX = "TOOLBOX";
 	public static final String DISCOVERY = "discovery";
 	public static final String CONFIGSERVER = "configServer";
-	public static final String TYPE ="type";
+	public static final String TYPE = "type";
 	public static final String GROUP = "group";
-	public static final String NAME = "name";
 	public static final String VERSION = "version";
-	public static final String OWN_HEALTH = "ownHealth";
-	public static final String LANE ="lane";
+	public static final String LANE = "lane";
 	public static final String ID = "id";
 	public static final String DETAILS = "details";
 	public static final String STATUS = "status";
-	public static final String VIRTUAL = "virtual";
-
+	public static final String DISK_SPACE = "diskSpace";
+	public static final String HYSTRIX = "hystrix";
 }
