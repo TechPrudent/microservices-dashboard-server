b0aefecdde8bcf93ec6efa7eac1996487f010186
andreasevers <andreas.evers@ordina.be>
2016-03-16 12:36:09 +0100

    Added CORS support

--- microservices-dashboard-server/src/main/java/com/pxs/dependencies/controllers/DependenciesController.java
+++ microservices-dashboard-server/src/main/java/com/pxs/dependencies/controllers/DependenciesController.java
@@ -10,15 +10,13 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.pxs.dependencies.model.Node;
 import com.pxs.dependencies.services.DependenciesResourceService;
 import com.pxs.dependencies.services.RedisService;
 
+@CrossOrigin(maxAge = 3600)
 @RestController
 public class DependenciesController {
 
