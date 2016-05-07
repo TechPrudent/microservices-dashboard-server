69e60e642e3bf5098ba48a85021ff6f771324ded
Tim Ysewyn <Tim.Ysewyn@ordina.be>
2016-05-07 22:32:56 +0200

    Added the abstract HealthAggregator

---
+++ microservices-dashboard-server/src/main/java/be/ordina/msdashboard/aggregator/HealthAggregator.java
@@ -0,0 +1,10 @@
+package be.ordina.msdashboard.aggregator;
+
+/**
+ * An abstract aggregator that can be used to compose nodes with their health information.
+ *
+ * @author Tim Ysewyn
+ */
+public abstract class HealthAggregator extends Aggregator {
+
+}
---
+++ microservices-dashboard-server/src/main/java/be/ordina/msdashboard/aggregator/health/Constants.java
@@ -0,0 +1,10 @@
+package be.ordina.msdashboard.aggregator.health;
+
+/**
+ * Constants used by the health aggregators
+ */
+public class Constants {
+
+    public final static String STATUS = "status";
+
+}
