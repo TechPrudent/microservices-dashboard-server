c2fd729b7e49d458e37d8c0aa54987b7f934c6b7
Tim Ysewyn <Tim.Ysewyn@ordina.be>
2016-05-16 13:26:51 +0200

    Added curies parsing

--- microservices-dashboard-server/src/main/java/be/ordina/msdashboard/aggregator/index/IndexesAggregator.java
+++ microservices-dashboard-server/src/main/java/be/ordina/msdashboard/aggregator/index/IndexesAggregator.java
@@ -5,10 +5,11 @@ import be.ordina.msdashboard.constants.Constants;
 import be.ordina.msdashboard.model.Node;
 import be.ordina.msdashboard.model.NodeBuilder;
 import be.ordina.msdashboard.model.Service;
-import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.ImmutableTriple;
 import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
 import org.apache.http.impl.nio.client.HttpAsyncClients;
 import org.apache.http.nio.client.methods.HttpAsyncMethods;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
@@ -20,6 +21,8 @@ import rx.Observable;
 import rx.apache.http.ObservableHttp;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.*;
@@ -88,7 +91,7 @@ public class IndexesAggregator extends EurekaBasedAggregator<Node> {
                                                                                                            .map(bytes -> {
                                                                                                                String response = new String(bytes);
                                                                                                                try {
-                                                                                                                   return new ImmutablePair<>(service, new JSONObject(response));
+                                                                                                                   return new ImmutableTriple<>(service, instance, new JSONObject(response));
                                                                                                                } catch (JSONException e) {
                                                                                                                    logger.error("An exception occurred: {}", e.getStackTrace());
                                                                                                                    logger.error("Response: {}", response);
@@ -97,23 +100,22 @@ public class IndexesAggregator extends EurekaBasedAggregator<Node> {
                                                                                                            }))
                                                   .filter(source -> Objects.nonNull(source));
                          })
-                         .map(pair -> {
+                         .map(triple -> {
                              NodeBuilder node = NodeBuilder.node()
-                                                           .withId(pair.getLeft());
+                                                           .withId(triple.getLeft());
 
-                             JSONObject source = pair.getRight();
+                             JSONObject source = triple.getRight();
 
                              if (!source.has(LINKS)) {
                                  logger.error("Index deserialization fails because no HAL _links was found at the root");
                              } else {
                                  JSONObject links = source.getJSONObject(LINKS);
-                                 boolean hasCuries = links.has(CURIES);
 
                                  ((Set<String>) links.keySet())
                                          .stream()
                                          .filter(linkKey -> !CURIES.equals(linkKey))
                                          .forEach(linkKey -> {
-                                             JSONObject link = source.getJSONObject(linkKey);
+                                             JSONObject link = links.getJSONObject(linkKey);
 
                                              NodeBuilder nodeBuilder = NodeBuilder.node()
                                                                                   .withId(linkKey)
@@ -122,7 +124,21 @@ public class IndexesAggregator extends EurekaBasedAggregator<Node> {
                                                                                   .withDetail("type", RESOURCE)
                                                                                   .withDetail("status", UP);
 
-                                             // TODO parse curies
+                                             if (links.has(CURIES)) {
+                                                 String namespace = linkKey.substring(0, linkKey.indexOf(":"));
+
+                                                 JSONArray curies = links.getJSONArray(CURIES);
+                                                 for (int i = 0; i < curies.length();) {
+                                                     JSONObject curie = curies.getJSONObject(i);
+
+                                                     if (curie.has(CURIE_NAME) && curie.getString(CURIE_NAME).equals(namespace)) {
+                                                         ServiceInstance instance = triple.getMiddle();
+                                                         String docs = instance.getUri().toString() + curie.getString(HREF).replace("{rel}", linkKey.substring(linkKey.indexOf(":") + 1));
+                                                         nodeBuilder.withDetail("docs", docs);
+                                                         break;
+                                                     }
+                                                 }
+                                             }
 
                                              node.withLinkedNode(nodeBuilder.build());
                                          });
