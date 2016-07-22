b4dc7101d4aa0210c1098bdceec1790738f73c15
Tim Ysewyn <Tim.Ysewyn@ordina.be>
2016-07-22 08:29:36 +0200

    Don't add cache annotations on interfaces - it is not recommended

--- microservices-dashboard-server/src/main/java/be/ordina/msdashboard/cache/NodeCache.java
+++ microservices-dashboard-server/src/main/java/be/ordina/msdashboard/cache/NodeCache.java
@@ -15,23 +15,17 @@
  */
 package be.ordina.msdashboard.cache;
 
-import be.ordina.msdashboard.constants.Constants;
-import org.springframework.cache.annotation.CacheEvict;
-
 /**
  * @author Andreas Evers
  */
 public interface NodeCache {
 
-    @CacheEvict(value = Constants.GRAPH_CACHE_NAME, allEntries = true)
     void evictGraphCache();
 
-    @CacheEvict(value = Constants.HEALTH_CACHE_NAME, allEntries = true)
     void evictHealthsCache();
 
-    @CacheEvict(value = Constants.INDEX_CACHE_NAME, allEntries = true)
     void evictIndexesCache();
 
-    @CacheEvict(value = Constants.PACTS_CACHE_NAME, allEntries = true)
     void evictPactsCache();
+
 }
